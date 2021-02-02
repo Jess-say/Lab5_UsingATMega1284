84ae5a7bad9145acca76e5690762ed756fedea6a
Jess-say <lu.jessie1714@gmail.com>
2021-02-01 23:21:12 -0800

    Completed part 1

--- turnin/jlu081_lab5_part1.c
+++ turnin/jlu081_lab5_part1.c
@@ -18,11 +18,8 @@ int main(void) {
 
 	unsigned char led = 0x00;
 	unsigned char button = 0x00;
-	//unsigned char A = 0x00;
-	//unsigned char C = 0x00;
     	while (1) {
 		button = ~PINA & 0x0F;
-		//A = PINA & 0xF0; 
 	 	led = 0x00;
 
 		// fuel tank is 1-2
@@ -47,41 +44,6 @@ int main(void) {
 		else {
 			led = 0x40;
 		}
-
-		// if key is in ignition. driver seated but belt not fastened
-		/*if (A == 0x30) {
-			C = 0x80;
-		}
-
-		A = PINA & 0x0F;
-
-		// PA3-PA0 holds values from 0-15
-		// if fuel level is 1 or 2 PC5 lights
-		if (A == 0x01 || A == 0x02) {
-			C =  C | 0x20 | 0x40;
-		}
-		// if fuel level is 3 or 4 
-		else if (A == 0x03 || A == 0x04) {
-			C = C | 0x30 | 0x40; // 0011 0000 and PC6 lights
-		}
-		// if fuel is 5 or 6 
-		else if (A == 0x05 || A == 0x06) {
-			C = C | 0x38; // 0011 1000
-		}
-		// if fuel is 7-9
-		else if (A >= 0x07 && A <= 0x09) {
-			C = C | 0x3C; // 0011 1100
-		}
-		// if fuel is 10-12
-		else if (A >= 0x0A && A <= 0x0C) {
-		       C = C | 0x3E; // 0011 1110
-		}
-		else if (A >= 0x0D && A <= 0x0F) {
-			C = C | 0x3F; // 0011 1111
-		}
-		else {
-			C = 0x40; // if fuel level is 0
-		}*/
 
 		PORTC = led;
     	}
